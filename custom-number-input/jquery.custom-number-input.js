d59596f0a00363c986d428656b6838472ee6631c
Alexander <alexander@sharapov.biz>
2017-09-12 23:59:50 +0600

    changing css and javascript

--- custom-number-input/jquery.custom-number-input.js
+++ custom-number-input/jquery.custom-number-input.js
@@ -6,11 +6,11 @@
 
 $.fn.cNumber = function (options) {
   var defaults = {
-    wrapperClass: "cNumber-input-group",
-    inputClass: "cNumber-input",
+    wrapperClass: "input-group",
+    inputClass: "input-number",
     min: "-10000000000000000",
     max: "10000000000000000",
-    controlButtonTemplate: '<div class="cNumber-nav"><div class="cNumber-btn cNumber-btn-up">&plus;</div><div class="cNumber-btn cNumber-btn-down">&minus;</div></div>'
+    controlButtonTemplate: '<div class="input-group-addon input-group-number-control"><div class="btn-plus">&plus;</div><div class="btn-minus">&minus;</div></div>'
   };
 
   options = $.extend(defaults, options);
@@ -20,11 +20,23 @@ $.fn.cNumber = function (options) {
         min = self.attr('min') || options.min,
         max = self.attr('max') || options.max,
         wrap = document.createElement('div');
-    wrap.className = options.wrapperClass;
-    self.wrap(wrap);
-    self.addClass('cNumber-input');
-    jQuery(options.controlButtonTemplate).insertAfter(self);
-    self.next().find('.cNumber-btn-up').click(function () {
+    if(self.closest('.input-group').length === 0) {
+      wrap.className = options.wrapperClass;
+      self.wrap(wrap);
+    }
+
+    self.addClass(options.inputClass);
+
+    jQuery(options.controlButtonTemplate)
+        .insertAfter(self);
+
+    if(self.hasClass('input-sm')) {
+      self.next('.input-group-number-control').addClass('input-group-number-control-sm');
+    } else if(self.hasClass('input-lg')) {
+      self.next('.input-group-number-control').addClass('input-group-number-control-lg');
+    }
+
+    self.next('.input-group-number-control').find('.btn-plus').click(function () {
       var ov = parseFloat(self.val());
       if(ov >= min && ov < max) {
         ov = ov + 1;
@@ -37,7 +49,7 @@ $.fn.cNumber = function (options) {
       self.trigger("change");
     });
 
-    self.next().find('.cNumber-btn-down').click(function () {
+    self.next('.input-group-number-control').find('.btn-minus').click(function () {
       var ov = parseFloat(self.val());
       if(ov > min && ov <= max) {
         ov = ov - 1;
